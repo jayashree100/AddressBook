8e6a175c3f33ca7d632d7e8fbf459c54edbf35e2
jayashree100 <sjayashree529@gmail.com>
2022-04-11 15:03:10 +0530

    first commit

---
+++ AddressBook/src/main/java/com/bridgelab/day28/AddressBookDirectoryIF.java
@@ -0,0 +1,25 @@
+package com.bridgelab.day28;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface AddressBookDirectoryIF {
+
+	public void addAddressBook();
+
+	public void operationDirectory();
+
+	public void displayDirectoryContents();
+
+	public void editAddressBook();
+
+	public void searchByCity();
+
+	public void searchByState();
+
+	public void displayPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
+
+	public void countPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
+
+	public void readDataFromJson();
+}
---
+++ AddressBook/src/main/java/com/bridgelab/day28/ContactPerson.java
@@ -0,0 +1,106 @@
+package com.bridgelab.day28;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class ContactPerson {
+
+	@CsvBindByName(column = "First Name")
+	private String firstName;
+
+	@CsvBindByName(column = "Last Name")
+	private String lastName;
+
+	@CsvBindByName(column = "Email")
+	private String email;
+
+	@CsvBindByName(column = "Phone Number")
+	private long phoneNumber;
+
+	@CsvBindByName(column = "City")
+	private String city;
+
+	@CsvBindByName(column = "State")
+	private String state;
+
+	@CsvBindByName(column = "Zip Code")
+	private long zip;
+
+	public ContactPerson() {
+		super();
+	}
+
+	public ContactPerson(String firstName, String lastName, String email, long phoneNumber, String city, String state,
+			long zip) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public long getZip() {
+		return zip;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+
+		return "First Name - " + firstName + ", Last Name - " + lastName + ", Phone Number - " + phoneNumber
+				+ ", Email - " + email + ", City - " + city + ", State - " + state + ", Zip Code - " + zip;
+	}
+}
